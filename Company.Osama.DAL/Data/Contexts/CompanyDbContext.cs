d57a4f1b681c1fa6c44ecfe71afb72f4e751f4a6
osama <o.omrann@storeus.com>
2026-04-05 17:02:18 +0200

    Implementation Of DAL

---
+++ Company.Osama.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Company.Osama.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Osama.DAL.Data.Contexts
+{
+    internal class CompanyDbContext: DbContext
+    {
+
+        public CompanyDbContext() : base()
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = OsamaCompany; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Company.Osama.DAL/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Osama.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public String Code { get; set; }
+        public String Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
