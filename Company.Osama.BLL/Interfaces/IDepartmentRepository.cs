20670e446e76d314ee7bd2fcb2beef8dcb7d1066
osama <o.omrann@storeus.com>
2026-04-10 16:06:10 +0200

    'save'

---
+++ Company.Osama.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Company.Osama.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Osama.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> getAll();
+
+        Department? get(int id);
+
+        int add(Department department);
+        int update(Department department);
+        int delete(Department department);
+    }
+}
---
+++ Company.Osama.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,49 @@
+﻿using Company.Osama.BLL.Interfaces;
+using Company.Osama.DAL.Data.Contexts;
+using Company.Osama.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Osama.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+        public int add(Department department)
+        {
+
+            _context.Departments.Add(department);
+            return _context.SaveChanges();
+        }
+
+        public int delete(Department department)
+        {   
+            _context.Departments.Remove(department);
+            return _context.SaveChanges();
+        }
+
+        public Department? get(int id)
+        { 
+            return _context.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> getAll()
+        {   
+            return _context.Departments.ToList();
+        }
+
+        public int update(Department department)
+        {
+            _context.Departments.Update(department);
+            return _context.SaveChanges();
+        }
+    }
+}
